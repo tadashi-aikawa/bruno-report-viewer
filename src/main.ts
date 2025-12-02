d301c3b33d6e9a4ec8255d7c8ac92c25a253d8c0
tadashi-aikawa <syou.maman@gmail.com>
2025-12-02 21:09:31 +0900

    refactor: 不要コード削除 & ts-pattern利用

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,16 @@
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import { match } from "ts-pattern";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 
 // @ts-ignore
 self.MonacoEnvironment = {
-  getWorker(_: any, label: string) {
-    if (label === "json") {
-      return new jsonWorker();
-    }
-    if (label === "css" || label === "scss" || label === "less") {
-      return new cssWorker();
-    }
-    if (label === "html" || label === "handlebars" || label === "razor") {
-      return new htmlWorker();
-    }
-    if (label === "typescript" || label === "javascript") {
-      return new tsWorker();
-    }
-    return new editorWorker();
-  },
+  getWorker: (_: any, label: string) =>
+    match(label)
+      .with("json", () => new jsonWorker())
+      .otherwise(() => new editorWorker()),
 };
 
 createApp(App).mount("#app");
