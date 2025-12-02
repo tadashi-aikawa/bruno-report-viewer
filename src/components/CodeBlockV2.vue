fb093a4c1f49d159497594d57c40c319817bc416
tadashi-aikawa <syou.maman@gmail.com>
2025-12-02 18:27:53 +0900

    style: エディタをsolarizedっぽいテーマにする

--- src/components/CodeBlockV2.vue
+++ src/components/CodeBlockV2.vue
@@ -13,11 +13,38 @@ const props = defineProps<{
 
 const editorEl = ref<HTMLDivElement | null>(null);
 onMounted(() => {
+  monaco.editor.defineTheme("solarized-light", {
+    base: "vs",
+    inherit: true,
+    rules: [
+      { token: "comment", foreground: "93a1a1" },
+      { token: "string", foreground: "2aa198" },
+      { token: "number", foreground: "d33682" },
+      { token: "keyword", foreground: "859900" },
+      { token: "identifier", foreground: "268bd2" },
+      { token: "type", foreground: "b58900" },
+      { token: "function", foreground: "268bd2" },
+      { token: "variable", foreground: "657b83" },
+      { token: "regexp", foreground: "2aa198" },
+      { token: "operator", foreground: "859900" },
+    ],
+    colors: {
+      "editor.background": "#fdf6e3",
+      "editor.foreground": "#657b83",
+      "editor.lineHighlightBackground": "#eee8d5",
+      "editorCursor.foreground": "#657b83",
+      "editorWhitespace.foreground": "#eee8d5",
+      "editorIndentGuide.background": "#eee8d5",
+      "editor.selectionBackground": "#eee8d5",
+    },
+  });
+
   monaco.editor.create(editorEl.value!, {
     value: props.content,
     language: "json",
     automaticLayout: true,
     readOnly: true,
+    theme: "solarized-light",
   });
 });
 </script>
